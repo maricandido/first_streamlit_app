8c4caf56cf26c470b0ad33404f3d98dbc3e9f7f7
MariaEduardaCandido <112425134+maricandido@users.noreply.github.com>
2024-01-03 14:26:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 #import pandas
 #import requests
 import snowflake.connector
-from urllib.error import ULRError
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Parents New Healthy Diner')
