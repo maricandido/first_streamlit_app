bd9fe0fa927c586d74a45d0ddb44f86df43dd005
MariaEduardaCandido <112425134+maricandido@users.noreply.github.com>
2024-01-03 13:34:06 -0300

    Update streamlit_app.py
    import snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 import requests
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 
