3c63fc5265f626f8ab92f497f526366405944b72
MariaEduardaCandido <112425134+maricandido@users.noreply.github.com>
2024-01-03 14:19:22 -0300

    Update streamlit_app.py
    Corringindo problemas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
-import pandas
+#import pandas
+#import requests
+import snowflake.connector
+import urllib.error import ULRError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-import requests
-
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -29,7 +30,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -39,4 +40,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 my_add_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cur = execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.write('Thanks for adding ', my_add_fruit)
